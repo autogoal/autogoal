9b3da140c93c5a23ca314aaa7ed502dd8325b5b4
Suilan Estévez <suilanestevez@gmail.com>
2019-12-27 15:12:33 +0100

    Agregando capa de Bert como clase

--- autogoal/ontology/_keras.py
+++ autogoal/ontology/_keras.py
@@ -121,3 +121,53 @@ def build_ontology_keras(onto):
 
             print(" -", arg, ":", typ.__name__)
             layer_instance.hasParameter.append(parameter)
+
+
+class BertLayer(tf.layers.Layer):
+    """ Permite utilizar Bert junto a queras y representa un embeding basado en Bert
+    """
+
+    def __init__(self, n_fine_tune_layers=10, **kwargs):
+        self.n_fine_tune_layers = n_fine_tune_layers
+        self.trainable = True
+        self.output_size = 768
+        super(BertLayer, self).__init__(**kwargs)
+
+    def build(self, input_shape):
+        self.bert = hub.Module(
+            bert_path,
+            trainable=self.trainable,
+            name="{}_module".format(self.name)
+        )
+        trainable_vars = self.bert.variables
+        
+        # Remove unused layers
+        trainable_vars = [var for var in trainable_vars if not "/cls/" in var.name]
+        
+        # Select how many layers to fine tune
+        trainable_vars = trainable_vars[-self.n_fine_tune_layers :]
+        
+        # Add to trainable weights
+        for var in trainable_vars:
+            self._trainable_weights.append(var)
+        
+        # Add non-trainable weights
+        for var in self.bert.variables:
+            if var not in self._trainable_weights:
+                self._non_trainable_weights.append(var)
+        
+        super(BertLayer, self).build(input_shape)
+
+    def call(self, inputs):
+        inputs = [K.cast(x, dtype="int32") for x in inputs]
+        input_ids, input_mask, segment_ids = inputs
+        bert_inputs = dict(
+            input_ids=input_ids, input_mask=input_mask, segment_ids=segment_ids
+        )
+        result = self.bert(inputs=bert_inputs, signature="tokens", as_dict=True)[
+            "pooled_output"
+        ]
+        return result
+
+    def compute_output_shape(self, input_shape):
+        return (input_shape[0], self.output_size)
