fbbe54ef039c68ffff2573f1029d0a1b854fb71f
Suilan Estévez <suilanestevez@gmail.com>
2019-12-30 01:29:40 +0100

    FIx bert

--- autogoal/ontology/_keras.py
+++ autogoal/ontology/_keras.py
@@ -8,6 +8,7 @@ from pathlib import Path
 
 import keras.layers
 import tensorflow_hub as hub
+import keras.backend as K
 
 from ._nn import (
     NeuralNetworkModule,
@@ -33,9 +34,10 @@ class Bert(keras.layers.Layer):
         super(Bert, self).__init__(**kwargs)
 
     def build(self, input_shape):
+        bert_path = "https://tfhub.dev/google/bert_uncased_L-12_H-768_A-12/1"
         self.bert = hub.Module(
             bert_path, trainable=self.trainable, name="{}_module".format(self.name)
-        )
+        ) 
         trainable_vars = self.bert.variables
 
         # Remove unused layers
@@ -53,7 +55,7 @@ class Bert(keras.layers.Layer):
             if var not in self._trainable_weights:
                 self._non_trainable_weights.append(var)
 
-        super(BertLayer, self).build(input_shape)
+        super(Bert, self).build(input_shape)
 
     def call(self, inputs):
         inputs = [K.cast(x, dtype="int32") for x in inputs]
