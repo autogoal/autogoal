0ee09acb64864767062dbe8e4e7ecc49e9aae74b
Suilan Estévez <suilanestevez@gmail.com>
2020-01-13 15:10:59 +0100

    incluyendo Bert

--- docs/examples/_bert.py
+++ docs/examples/_bert.py
@@ -1,9 +1,59 @@
 
+import keras
+
 from keras.layers import Input, Dense
 from keras.models import Model
-from autogoal.ontology._keras import Bert
-
 from keras_bert import Tokenizer, load_vocabulary
+
+
+class Bert(keras.layers.Layer):
+    """ Permite utilizar Bert junto a keras y representa un embeding basado en Bert
+    """
+
+    def __init__(self, n_fine_tune_layers=10, **kwargs):
+        self.n_fine_tune_layers = n_fine_tune_layers
+        self.trainable = True
+        self.output_size = 768
+        super(Bert, self).__init__(**kwargs)
+
+    def build(self, input_shape):
+        bert_path = "https://tfhub.dev/google/bert_uncased_L-12_H-768_A-12/1"
+        self.bert = hub.Module(
+            bert_path, trainable=self.trainable, name="{}_module".format(self.name)
+        )
+        trainable_vars = self.bert.variables
+
+        # Remove unused layers
+        trainable_vars = [var for var in trainable_vars if not "/cls/" in var.name]
+
+        # Select how many layers to fine tune
+        trainable_vars = trainable_vars[-self.n_fine_tune_layers :]
+
+        # Add to trainable weights
+        for var in trainable_vars:
+            self._trainable_weights.append(var)
+
+        # Add non-trainable weights
+        for var in self.bert.variables:
+            if var not in self._trainable_weights:
+                self._non_trainable_weights.append(var)
+
+        super(Bert, self).build(input_shape)
+
+    def call(self, inputs):
+        inputs = [K.cast(x, dtype="int32") for x in inputs]
+        input_ids, input_mask, segment_ids = inputs
+        bert_inputs = dict(
+            input_ids=input_ids, input_mask=input_mask, segment_ids=segment_ids
+        )
+        result = self.bert(inputs=bert_inputs, signature="tokens", as_dict=True)[
+            "pooled_output"
+        ]
+        return result
+
+    def compute_output_shape(self, input_shape):
+        return (input_shape[0], self.output_size)
+
 
 def main():
     layer_num = 12
