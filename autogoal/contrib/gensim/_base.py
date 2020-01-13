3a242529e709382ac81b3f13c9c6a2ac3e39a9bc
Alejandro Piad <alepiad@gmail.com>
2020-01-13 16:35:36 +0100

    Update W2V

--- autogoal/contrib/gensim/_base.py
+++ autogoal/contrib/gensim/_base.py
@@ -3,24 +3,19 @@ import gensim.downloader as api
 model = api.load("glove-twitter-25")
 
 class Word2VectEmbedding: 
-    """This class transform a word in embedding vector using Word2Vect of gensim (using glove-twitter-25).
+    """This class transform a word in embedding vector using Word2Vect of `gensim` (using `glove-twitter-25`).
 
-        ##### Notes
+    ##### Notes
 
-        On the first use the model Word2Vect of gensim will be downloaded. This may take a few minutes.
-        
-        If you are using the development container the model should be already downloaded for you.
+    On the first use the model Word2Vect of gensim will be downloaded. This may take a few minutes.
+    
+    If you are using the development container the model should be already downloaded for you.
     """
 
     def __init__(self):
         self.model = api.load("glove-twitter-25")
 
-    def convert(self, word):
+    def run(self, input:"Word")-> "Vector": 
         """This method use Word2Vect of gensim for tranform a word in embedding vector. 
         """
         return self.model.get_vector(word)
-
-    def run(self, input:"Word")-> "Vector": 
-        return convert(input)
-
-
