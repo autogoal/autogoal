b7d3916338e9fbe89c89e2b5fee6832169fee1a8
Alejandro Piad <alepiad@gmail.com>
2020-01-21 20:52:52 +0100

    Better repr() using black

--- autogoal/utils/__init__.py
+++ autogoal/utils/__init__.py
@@ -41,8 +41,13 @@ def nice_repr(cls):
         parameter_names = [name for name in init_signature.parameters if name not in exclude_param_names]
         parameter_values = [getattr(self, param, None) for param in parameter_names]
         args = ", ".join(f"{name}={repr(value)}" for name, value in zip(parameter_names, parameter_values) if value)
+        fr = f"{cls.__name__}({args})"
 
-        return f"{cls.__name__}({args})"
+        try:
+            import black
+            return black.format_str(fr, mode=black.FileMode()).strip()
+        except:
+            return fr
 
     cls.__repr__ = repr_method
     return cls
