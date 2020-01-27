21240c4592355014e667f133895806d2ad81963f
Suilan Estevez <suilanestevez@gmail.com>
2020-01-27 10:50:37 +0100

    add spanish enmbedding

--- autogoal/contrib/gensim/_base.py
+++ autogoal/contrib/gensim/_base.py
@@ -23,3 +23,34 @@ class Word2VecEmbedding:
         """This method use Word2Vect of gensim for tranform a word in embedding vector.
         """
         return self.model.get_vector(input)
+    
+
+@nice_repr
+class Word2VecEmbeddingSpanish:
+    """This class transform a word in embedding vector using Word2Vect of `gensim` (using `Spanish 3B words Word2Vec`).
+
+    ##### Notes
+
+    On the first use the model Word2Vect of gensim will be downloaded. This may take a few minutes.
+
+    If you are using the development container the model should be already downloaded for you.
+    """            
+    def _load_model(self):
+        url = "https://zenodo.org/record/1410403/files/keyed_vectors.zip?download=1"
+        path = Path(__file__).parent / 'spanish-w2v.kv'
+
+        if not path.exists():
+            with path.open("wb") as fp:
+                stream = requests.get(url)
+                fp.write(stream.content)
+
+        return KeyedVectors.load('complete.kv', mmap='r')
+    
+    @property
+    def model(self) -> KeyedVectors:    
+        return CacheManager.instance().get('spanish-w2v', self._load_model)
+
+    def run(self, input: Word(domain='general', language='spanish'))-> ContinuousVector():
+        """This method use Word2Vect of gensim for tranform a word in embedding vector.
+        """
+        return self.model.get_vector(input)
