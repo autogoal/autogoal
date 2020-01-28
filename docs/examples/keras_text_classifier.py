c33cc2a58a77bed64c32a02d055e1699a99a14ca
Alejandro Piad <alepiad@gmail.com>
2020-01-28 14:56:01 +0100

    Testing HAHA

--- docs/examples/keras_text_classifier.py
+++ docs/examples/keras_text_classifier.py
@@ -1,26 +1,37 @@
 from autogoal.contrib.keras import KerasSequenceClassifier
 from autogoal.contrib.torch import BertEmbedding
+from autogoal.datasets import haha
+from autogoal.kb import CategoricalVector, List, Sentence, Tuple
+from autogoal.ml import AutoClassifier
+from autogoal.search import ConsoleLogger, ProgressLogger
 
-from autogoal.datasets import movie_reviews
-from autogoal.search import RandomSearch
 
-import numpy as np
-
-bert = BertEmbedding(32)
-x = bert.run(
-    [
-        "this is an english sentence for bert",
-        "another sentence",
-        "a longer sentence that some words will be removed for sure, not really, but now it is",
-    ]
+classifier = AutoClassifier(
+    input=List(Sentence()),
+    registry=[KerasSequenceClassifier, BertEmbedding],
+    search_kwargs=dict(memory_limit=0, evaluation_timeout=0),
 )
 
-print(x.shape)
 
-classifier = KerasSequenceClassifier(None, 768)
+Xtrain, Xtest, ytrain, ytest = haha.load()
 
-# for i in range(10):
-classifier.sample()
-classifier.model.summary()
+classifier.fit(Xtrain, ytrain, logger=[ConsoleLogger(), ProgressLogger()])
 
-classifier.fit(x, np.asarray([True, False, True]))
+# bert = BertEmbedding(32)
+# x = bert.run(
+#     [
+#         "this is an english sentence for bert",
+#         "another sentence",
+#         "a longer sentence that some words will be removed for sure, not really, but now it is",
+#     ]
+# )
+
+# print(x.shape)
+
+# classifier = KerasSequenceClassifier(None, 768)
+
+# # for i in range(10):
+# classifier.sample()
+# classifier.model.summary()
+
+# classifier.fit(x, np.asarray([True, False, True]))
