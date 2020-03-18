52e7d240d6de6e816381f903b102e64896c2277d
Alejandro Piad <alepiad@gmail.com>
2020-03-18 17:29:07 +0100

    Add cifar10 dataset

---
+++ autogoal/datasets/cifar10.py
@@ -0,0 +1,51 @@
+from autogoal.datasets import download, datapath
+import pickle
+import numpy as np
+
+
+def load(training_batches=5):
+    """
+    Load the CIFAR-10 dataset
+
+    ##### Parameters
+
+    * 'training_batches': maximum number of batches to load for training, 
+      each batch has 10,000 examples (min=`1`, max=`5`, default=`5`).
+
+    ##### Examples
+
+    >>> X_train, y_train, X_test, y_test = load(training_batches=1)
+    >>> X_train.shape
+    (10000, 3072)
+    >>> len(y_train)
+    10000
+    >>> X_test.shape
+    (10000, 3072)
+    >>> len(y_test)
+    10000
+    >>> y_train[0]
+    6
+
+    """
+    download("cifar10")
+
+    X_train = []
+    y_train = []
+
+    for i in range(1, training_batches + 1):
+        batch = datapath('cifar10') / f'data_batch_{i}'
+
+        with open(batch, 'rb') as fp:
+            data = pickle.load(fp, encoding='bytes')
+            X_train.append(data[b'data'])
+            y_train.extend(data[b'labels'])
+
+    X_train = np.vstack(X_train)
+
+    test_batch = datapath('cifar10') / 'test_batch'
+
+    with open(test_batch, 'rb') as fp:
+        data = pickle.load(fp, encoding='bytes')
+        X_test, y_test = data[b'data'], data[b'labels']
+
+    return X_train, y_train, X_test, y_test
