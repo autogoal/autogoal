78cb5ec6d901b9a0e9efc12e8226f65914e0baa8
Hieu Do Ngoc <hieudn.uh@gmail.com>
2020-07-03 17:41:33 -0400

    Add experiments

---
+++ scripts/cifar10_exp.py
@@ -0,0 +1,30 @@
+import numpy as np
+
+from autogoal.ml import AutoML
+from autogoal.contrib.keras_hierarchical import KerasImageClassifier
+from autogoal.datasets import cifar10
+from autogoal.kb import CategoricalVector, Tensor4
+from autogoal.search import ConsoleLogger, ProgressLogger
+from autogoal.utils import Gb, Hour
+
+automl = AutoML(
+    input=Tensor4(),
+    output=CategoricalVector(),
+    registry=[KerasImageClassifier],
+    cross_validation_steps=1,
+    search_kwargs=dict(
+        pop_size=30,
+        search_timeout= 3 * 24 * Hour,
+        evaluation_timeout=0,
+        memory_limit=60 * Gb,
+        save=False,
+    ),
+    search_iterations=1000,
+    validation_split=1/6
+)
+
+Xtrain, ytrain, Xtest, ytest = cifar10.load()
+X = np.vstack((Xtrain, Xtest))
+y = np.hstack((ytrain, ytest))
+
+automl.fit(X, y, logger=[ConsoleLogger(), ProgressLogger()])
---
+++ scripts/mnist_exp.py
@@ -0,0 +1,27 @@
+from autogoal.ml import AutoML
+from autogoal.contrib.keras_hierarchical import *
+from autogoal.kb import *
+from autogoal.search import ConsoleLogger, ProgressLogger
+from autogoal.utils import Gb, Hour
+
+from autogoal.datasets import mnist
+
+automl = AutoML(
+    input=Tensor4(),
+    output=CategoricalVector(),
+    registry=[KerasImageClassifier],
+    cross_validation_steps=1,
+    search_kwargs=dict(
+        pop_size=30,
+        search_timeout=3 * 24 * Hour,
+        evaluation_timeout=0,
+        memory_limit=60 * Gb,
+        save=False,
+    ),
+    search_iterations=1000,
+    validation_split=1/6
+)
+
+Xtrain, ytrain, xt, yt = mnist.load(unrolled=False)
+
+automl.fit(Xtrain, ytrain, logger=[ConsoleLogger(), ProgressLogger()])
