632b6212948fa4a12887ae0f00a734f1087f3d3b
Alejandro Piad <alepiad@gmail.com>
2020-10-11 09:54:13 +0000

    Add contrib CLI option

--- autogoal/__main__.py
+++ autogoal/__main__.py
@@ -1,36 +1,66 @@
-import fire
+import typer
+import collections
 
 from pathlib import Path
 from autogoal import kb
 from autogoal.contrib import find_classes
 
 
+app = typer.Typer(name="AutoGOAL")
+contrib_app = typer.Typer(name="contrib")
+
+app.add_typer(contrib_app)
+
+
+@app.callback()
+def main():
+    """
+    Manage AutoGOAL directly from the CLI.
+    """
+
+
+@app.command()
 def demo():
     try:
         from streamlit.bootstrap import run
+
         run(Path(__file__).parent / "contrib" / "streamlit" / "demo.py", "", "")
     except ImportError:
         print("(!) Too run the demo you need streamlit installed.")
         print("(!) Fix it by running `pip install autogoal[streamlit]`.")
 
 
-def graph(input:str, output:str):
-    input_type = eval(input, kb.__dict__)
+@contrib_app.callback()
+def contrib_main():
+    """
+    Inspect contrib libraries and algorithms.
+    """
 
-    print(input_type)
 
+@contrib_app.command("list")
+def contrib_list(verbose: bool = False, include: str = None, exclude: str = None):
+    """
+    List all currently available contrib algorithms.
+    """
+    from autogoal.contrib import find_classes
 
-def main():
-    from autogoal.datasets import pack, unpack, download
+    classes = find_classes(include=include, exclude=exclude)
+    classes_by_contrib = collections.defaultdict(list)
 
-    fire.Fire(dict(
-        pack=pack,
-        unpack=unpack,
-        download=download,
-        demo=demo,
-        graph=graph,
-    ), name='autogoal')
+    for cls in classes:
+        classes_by_contrib[str(cls).split(".")[2]].append(cls)
+
+    typer.echo(
+        f"⚙️  Found a total of {len(classes)} matching algorithms.", color="blue"
+    )
+
+    for contrib, clss in classes_by_contrib.items():
+        typer.echo(f"🛠️  {contrib}: {len(clss)} algorithms.")
+
+        if verbose:
+            for cls in clss:
+                typer.echo(f" 🔹 {cls.__name__}")
 
 
 if __name__ == "__main__":
-    main()
+    app()
--- autogoal/contrib/__init__.py
+++ autogoal/contrib/__init__.py
@@ -1,4 +1,4 @@
-def find_classes(include=".*", exclude=None, modules=None):
+def find_classes(include=None, exclude=None, modules=None):
     import inspect
     import warnings
     import re
@@ -20,74 +20,46 @@ def find_classes(include=".*", exclude=None, modules=None):
             from autogoal.contrib import sklearn
             modules.append(sklearn)
         except ImportError as e:
-            warnings.warn(repr(e))
-            warnings.warn(
-                "Skipping `scikit-learn`. Run `pip install autogoal[sklearn]` to include it."
-            )
             pass
 
         try:
             from autogoal.contrib import nltk
             modules.append(nltk)
         except ImportError as e:
-            warnings.warn(repr(e))
-            warnings.warn(
-                "Skipping `nltk`. Run `pip install autogoal[nltk]` to include it."
-            )
             pass
 
         try:
             from autogoal.contrib import gensim
             modules.append(gensim)
         except ImportError as e:
-            warnings.warn(repr(e))
-            warnings.warn(
-                "Skipping `gensim`. Run `pip install autogoal[gensim]` to include it."
-            )
             pass
 
         try:
             from autogoal.contrib import keras
             modules.append(keras)
         except ImportError as e:
-            warnings.warn(repr(e))
-            warnings.warn(
-                "Skipping `keras`. Run `pip install autogoal[keras]` to include it."
-            )
             pass
 
         try:
             from autogoal.contrib import torch
             modules.append(torch)
         except ImportError as e:
-            warnings.warn(repr(e))
-            warnings.warn(
-                "Skipping `torch`. Run `pip install autogoal[torch]` to include it."
-            )
             pass
 
         try:
             from autogoal.contrib import spacy
             modules.append(spacy)
         except ImportError as e:
-            warnings.warn(repr(e))
-            warnings.warn(
-                "Skipping `spacy`. Run `pip install autogoal[spacy]` to include it."
-            )
             pass
 
         try:
             from autogoal.contrib import wikipedia
             modules.append(wikipedia)
         except ImportError as e:
-            warnings.warn(repr(e))
-            warnings.warn(
-                "Skipping `wikipedia`. Run `pip install autogoal[wikipedia]` to include it."
-            )
             pass
 
-        from autogoal.contrib import _wrappers
-        modules.append(_wrappers)
+        from autogoal.contrib import wrappers
+        modules.append(wrappers)
 
         from autogoal.contrib import regex
         modules.append(regex)
@@ -95,6 +67,9 @@ def find_classes(include=".*", exclude=None, modules=None):
     for module in modules:
         for _, cls in inspect.getmembers(module, inspect.isclass):
             if not hasattr(cls, 'run'):
+                continue
+
+            if cls.__name__.startswith("_"):
                 continue
 
             if not re.match(include, repr(cls)):
