56b58cdb08acf11f63f14acc9de73dd3d89096bc
Alejandro Piad Morffis <apiad@apiad.net>
2021-01-12 08:54:02 -0500

    Add example script for documentation

---
+++ docs/shell/autogoal_example.py
@@ -0,0 +1,21 @@
+# import high-level API
+from autogoal.ml import AutoML
+from autogoal.kb import MatrixContinuousDense, CategoricalVector
+
+# load data
+from autogoal.datasets import cars
+X, y = cars.load()
+
+# instantiate AutoML class
+automl = AutoML(
+    input=MatrixContinuousDense(),
+    output=CategoricalVector(),
+    # ... other parameters and constraints
+)
+
+# fit the model
+automl.fit(X, y)
+
+# save the best model
+with open("model.bin", "wb") as fp:
+    automl.save(fp)
---
+++ docs/shell/printscript.py
@@ -0,0 +1,28 @@
+import sys
+from sys import argv
+from rich.console import Console
+from rich.syntax import Syntax
+from rich.text import Text
+
+import time
+
+console = Console()
+console.print("")
+
+with open(sys.argv[1]) as fp:
+    text = fp.read()
+    styled_text = Syntax("", lexer_name="Python", theme="monokai", line_numbers=True).highlight(text)
+
+time.sleep(0.5)
+
+for i, c in enumerate(text):
+    console.print(c, end="", style=styled_text.get_style_at_offset(console, i))
+
+    if c == " ":
+        time.sleep(0.2)
+    elif c == "\n":
+        time.sleep(0.5)
+    else:
+        time.sleep(0.1)
+
+console.print("\n")
