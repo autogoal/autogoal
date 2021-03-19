0bfa28fe801dc4afbf380aab1677fe76d0815ff2
Alejandro Piad Morffis <apiad@apiad.net>
2021-03-19 12:53:09 +0000

    Add another test

--- tests/contrib/test_specific_pipelines.py
+++ tests/contrib/test_specific_pipelines.py
@@ -1,7 +1,8 @@
 from autogoal.kb import Pipeline, Seq, Sentence, Tensor, Dense, Categorical
-from autogoal.contrib.sklearn import CountVectorizerNoTokenize
+from autogoal.experimental.pipeline import Supervised
+from autogoal.contrib.sklearn import CountVectorizerNoTokenize, CountVectorizer
 from autogoal.contrib.nltk._generated import SExprTokenizer, MWETokenizer
-from autogoal.contrib.sklearn._generated import ExtraTreeClassifier
+from autogoal.contrib.sklearn._generated import ExtraTreeClassifier, SGDClassifier
 
 
 def test_count_vectorizer_extra_trees_classifier():
@@ -22,10 +23,35 @@ def test_count_vectorizer_extra_trees_classifier():
                 ccp_alpha=0.0,
             ),
         ],
-        input_types=(Seq[Sentence], Tensor[1, Categorical, Dense]),
+        input_types=(Seq[Sentence], Supervised[Tensor[1, Categorical, Dense]]),
     )
 
     Xtrain = ["hello world", "this is sparta"]
     ytrain = ["true", "false"]
 
     p.run(Xtrain, ytrain)
+
+
+def test_count_vectorizer_sgd():
+    p = Pipeline(
+        algorithms=[
+            CountVectorizer(lowercase=True, binary=True),
+            SGDClassifier(
+                loss="perceptron",
+                penalty="l1",
+                l1_ratio=0.999,
+                fit_intercept=False,
+                tol=0.001,
+                shuffle=True,
+                epsilon=0.24792790326293826,
+                learning_rate="optimal",
+                eta0=0.992,
+                power_t=4.991,
+                early_stopping=False,
+                validation_fraction=0.993,
+                n_iter_no_change=1,
+                average=True,
+            ),
+        ],
+        input_types=(Seq[Sentence], Supervised[Tensor[1, Categorical, Dense]]),
+    )
