c0a139ed776750144c1948c03823457808bbb689
Alejandro Piad Morffis <apiad@apiad.net>
2021-04-03 18:49:28 -0400

    Update documentation for autgoal.__init__

--- autogoal/__init__.py
+++ autogoal/__init__.py
@@ -11,15 +11,36 @@ tunable, and then use search algorithms to find the best way to tune or configur
 # By default, Python won't import submodules, hence, if we want `autogoal.*` to work we'll have
 # to import all submodules here manually.
 
-from autogoal import contrib
-from autogoal import datasets
+# These four submodules include the low-level components of AutoGOAL
+# from which we can build all the core functionality.
+
+# The [`grammar`](ref:autogoal.grammar.__init__)
+# and [`sampling`](ref:autogoal.sampling.__init__) submodules allows us to
+# define search spaces with arbitrary structure,
+# and automatically create instances of different types of objects by sampling from them.
 from autogoal import grammar
-from autogoal import kb
-from autogoal import ml
 from autogoal import sampling
+
+# The [`kb`](ref:autgoal.kb.__init__) submodule allows us to
+# define algorithms based on input and output types that
+# and combine them automatically intro pipeline graphs.
+from autogoal import kb
+
+# The [`search`](ref:autogoal.search.__init__) submodule contains search
+# strategies to optimize in different hyper-parameter spaces.
 from autogoal import search
+
+# With these low level structures, we can build the core functionality of AutoGOAL, the AutoML algorithm.
+# The [`ml`](ref:autogoal.ml.__init__) submodule contains the definition of the [`AutoML`](ref:autogoal.ml._automl:AutoML) class
+# and related functionality.
+from autogoal import ml
+
 from autogoal import utils
 from autogoal import logging
+
+from autogoal import datasets
+
+from autogoal import contrib
 
 from autogoal.utils._helpers import optimize
 
