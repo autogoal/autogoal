50de5d84abaa2c88704267b8cd30b91bb5156fc3
Mauricio Perdomo <ml.perdomo97@gmail.com>
2021-06-04 22:34:27 -0400

    Move new semantic types to experimental

---
+++ autogoal/experimental/audio_command_recognition/kb/_semantics.py
@@ -0,0 +1,22 @@
+from autogoal.kb._semantics import SemanticType, Tensor, Continuous, Dense
+from os import path
+
+# Semantics types for audio processing
+class AudioFile(SemanticType):
+    '''Semantic type for wav audio files'''
+    @classmethod
+    def _match(cls, x):
+        try:
+            return path.isfile(x) and x.split('.')[-1] == 'wav'
+        except TypeError:
+            return False
+
+
+class AudioCommand(SemanticType):
+    '''Semantic type for vectors representing audio commands'''
+    @classmethod
+    def _match(cls, x):
+        pass
+
+
+AudioFeatures = Tensor[3, Continuous, Dense]
--- autogoal/kb/_semantics.py
+++ autogoal/kb/_semantics.py
@@ -483,25 +483,6 @@ Tensor3 = Tensor[3, Continuous, Dense]
 Tensor4 = Tensor[4, Continuous, Dense]
 
 
-# Semantics types for audio processing
-class AudioFile(SemanticType):
-    '''Semantic type for wav audio files'''
-    @classmethod
-    def _match(cls, x):
-        try:
-            return path.isfile(x) and x.split('.')[-1] == 'wav'
-        except TypeError:
-            return False
-
-
-class AudioCommand(SemanticType):
-    '''Semantic type for vectors representing audio commands'''
-    @classmethod
-    def _match(cls, x):
-        pass
-
-
-AudioFeatures = Tensor[3, Continuous, Dense]
 
 
 # Finally we define the publicly export classes
@@ -538,9 +519,6 @@ __all__ = [
     "Categorical",
     "Continuous",
     "Discrete",
-    "AudioFile",
-    "AudioCommand",
-    "AudioFeatures"
 ]
 
 
