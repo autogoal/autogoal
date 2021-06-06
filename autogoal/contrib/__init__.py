9e89add38a2d3ca1feda576b2959ef2b85224aef
Mauricio Perdomo <ml.perdomo97@gmail.com>
2021-06-06 00:28:07 -0400

    Bring contrib/__init__.py from main branch

--- autogoal/contrib/__init__.py
+++ autogoal/contrib/__init__.py
@@ -70,7 +70,6 @@ def find_classes(include=None, exclude=None, modules=None, input=None, output=No
         except ImportError as e:
             pass
 
-
         from autogoal.contrib import wrappers
 
         modules.append(wrappers)
