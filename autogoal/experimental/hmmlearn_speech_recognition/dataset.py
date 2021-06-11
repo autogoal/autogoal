2dd3fa0758d134ce577b6dc17975d1535fbcbd06
Antoniojesus0398 <otano0398@gmail.com>
2021-06-10 21:56:15 -0400

    Create method for downloading the example dataset and loading it

--- autogoal/experimental/hmmlearn_speech_recognition/dataset.py
+++ autogoal/experimental/hmmlearn_speech_recognition/dataset.py
@@ -1,11 +1,30 @@
 from os import listdir
+from autogoal.datasets import download_and_save, datapath, unpack
 
-root_dir = './autogoal/experimental/hmmlearn_speech_recognition'
+data_url = 'https://github.com/AntonioJesus0398/speech-recognition-dataset/raw/master/speech-recognition-dataset.zip'
+
+dataset = 'speech-recognition-dataset'   
 
 def load():
+    # Download the data
+    fname = f'{dataset}.zip'
+    file_path = datapath(fname)
+    if not file_path.exists():
+        print(f'Downloading {dataset}...')
+        download_and_save(data_url, file_path, True)
+        print('Done!\n')
+    
+    # Unpack the zip file
+    dir_path = datapath(dataset)
+    if not dir_path.exists():
+        print(f'Unpacking {fname}...')
+        unpack(fname)
+        print('Done.\n')
+
+    # build the training and test sets
     X_train, y_train, X_test, y_test = [], [], [], []
-    for subfolder in listdir(f'{root_dir}/data'):
-        base_path = f'{root_dir}/data/{subfolder}/'
+    for subfolder in listdir(str(dir_path)):
+        base_path = f'{dir_path}/{subfolder}/'
         for audio_file in listdir(base_path)[:-1]:
             file_path = base_path + audio_file
             X_train.append(file_path)
--- autogoal/experimental/hmmlearn_speech_recognition/example.py
+++ autogoal/experimental/hmmlearn_speech_recognition/example.py
@@ -3,7 +3,7 @@ from autogoal.kb import *
 from autogoal.search import RichLogger
 from autogoal.utils import Min, Gb
 from autogoal.experimental.hmmlearn_speech_recognition.util import AudioFile
-from autogoal.experimental.hmmlearn_speech_recognition._manual import HMMLearnSpeechRecognizer
+from autogoal.experimental.hmmlearn_speech_recognition.main import HMMLearnSpeechRecognizer
 from autogoal.contrib import find_classes
 import numpy as np
 from os import listdir
@@ -13,14 +13,17 @@ automl = AutoML(
     input=(Seq[AudioFile], Supervised[Seq[Word]]),
     output=Seq[Word],
     registry=[HMMLearnSpeechRecognizer] + find_classes(),
-    evaluation_timeout= 2 * Min,
+    evaluation_timeout= Min,
     memory_limit= 4 * Gb,
-    search_timeout= 5 * Min
+    search_timeout= Min
 )
 
 X_train, y_train, X_test, y_test = load()
+
 automl.fit(X_train, y_train, logger=[RichLogger()])
+
 score = automl.score(X_test, y_test)
 print(f'Score: {score}' )
+
 answers = automl.predict(X_test)
 print(f'Answers: {answers}')
