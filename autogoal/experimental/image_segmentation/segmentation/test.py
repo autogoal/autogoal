3fecf8feb93f14483d0bc0c5617f905f5c6a516f
Isabella Sierra <tuquisierra@gmail.com>
2021-06-17 21:23:42 -0400

    adding preprocessor to pipeline

--- autogoal/experimental/image_segmentation/segmentation/test.py
+++ autogoal/experimental/image_segmentation/segmentation/test.py
@@ -3,14 +3,14 @@ from autogoal.kb import *
 from ._semantics import Image
 from autogoal.contrib import find_classes
 from ..keras._base import KerasImageSegmenter
-from ._base import ImageSegmenter
+from ._base import ImageSegmenter, ImagePreprocessor
 from ..data.dataset import load
 from autogoal.search import RichLogger
 
 
 def test():
     automl = AutoML(input=(Seq[Image], Seq[Image]), output=Seq[Image], cross_validation_steps=1,
-                    registry=find_classes() + [ImageSegmenter, KerasImageSegmenter])
+                    registry=find_classes() + [ImageSegmenter, KerasImageSegmenter, ImagePreprocessor])
     
     x_train, y_train, x_test, y_test = load()
 
