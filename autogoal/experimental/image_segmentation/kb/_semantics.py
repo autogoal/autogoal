5dd5201edd0a49646b12da73c71e15962c4c83c7
stdevAdrianPaez <adriantpaez@gmail.com>
2021-06-17 20:10:04 -0400

    Add ImageFile

---
+++ autogoal/experimental/image_segmentation/kb/_semantics.py
@@ -0,0 +1,13 @@
+from autogoal.kb._semantics import SemanticType
+from os import path
+
+
+class ImageFile(SemanticType):
+    """Semantic type for JPG image files"""
+
+    @classmethod
+    def _match(cls, x):
+        try:
+            return path.isfile(x) and x.split(".")[-1] == "jpg"
+        except TypeError:
+            return False
