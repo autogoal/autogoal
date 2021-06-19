bd4115b516e5ea058ba6d58e8503a3c6fe8adcd1
dgc9715 <dgc9715@gmail.com>
2021-06-19 23:19:43 +0000

    add SupervisedTextMatcher test basis

---
+++ tests/experimental/deepmatcher/test.py
@@ -0,0 +1,37 @@
+from autogoal.kb import Sentence, Seq, Supervised ,VectorCategorical
+from autogoal.ml import AutoML
+from autogoal.contrib import find_classes
+from autogoal.utils import Min, Gb
+from autogoal.kb import Seq, Word, Sentence, MatrixContinuousDense, build_pipeline_graph
+from autogoal.contrib import find_classes
+from autogoal.experimental.deepmatcher.base import SupervisedTextMatcher
+from autogoal.search import RichLogger
+
+def test_supervised_text_matcher():
+    from autogoal.experimental.deepmatcher.datasets.text_matching import load
+    X_train, y_train , X_test , y_test = load()
+
+    automl = AutoML(
+        input = (Seq[Sentence], Supervised[VectorCategorical]), # fix annotations
+        output = VectorCategorical, # fix annotations
+        registry = [SupervisedTextMatcher] + find_classes(),
+        evaluation_timeout = 3 * Min,
+        memory_limit = 3 * Gb,
+        search_timeout = 10 * Min
+    )
+
+    automl.fit(X_train,y_train,logger=RichLogger())
+    score = automl.score(X_test, y_test)
+    print(score)
+
+def test_supervised_text_matcher_pipeline():
+    pipelines = build_pipeline_graph(
+        input_types = (Seq[Sentence], Seq[Word]),  # fix annotations
+        output_type = MatrixContinuousDense,  # fix annotations
+        registry = find_classes() + [SupervisedTextMatcher],
+    )
+    nodes = pipelines.nodes()
+    assert SupervisedTextMatcher in nodes
+
+if __name__ == '__main__':
+    test_supervised_text_matcher()
