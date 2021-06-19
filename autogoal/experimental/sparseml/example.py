681231aef723a648c7722b9fbf1422dd79581179
LSilvaO <lisetsilvao2003@gmail.com>
2021-06-19 03:07:43 +0000

    Format files

--- autogoal/experimental/sparseml/example.py
+++ autogoal/experimental/sparseml/example.py
@@ -4,23 +4,23 @@ from autogoal.kb import *
 from autogoal.contrib import find_classes
 from autogoal.experimental.sparseml.main import build_sparseml_keras_classifier
 
-classifier=AutoML(
-    input=(MatrixContinuousDense,Supervised[VectorCategorical]),
+classifier = AutoML(
+    input=(MatrixContinuousDense, Supervised[VectorCategorical]),
     output=VectorCategorical,
     cross_validation_steps=1,
-    registry=[build_sparseml_keras_classifier('recipe.yaml')],
+    registry=[build_sparseml_keras_classifier("recipe.yaml")],
 )
 
-loggers=[RichLogger()]
+loggers = [RichLogger()]
 
 from autogoal.datasets import cars
 from sklearn.model_selection import train_test_split
 
-X, y =cars.load()
-X_train, X_test, y_train, y_test = train_test_split(X,y,test_size=0.2)
+X, y = cars.load()
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
 
-classifier.fit(X_train,y_train,logger=loggers)
+classifier.fit(X_train, y_train, logger=loggers)
 
-score=classifier.score(X_test,y_test)
+score = classifier.score(X_test, y_test)
 
 print(score)
--- autogoal/experimental/sparseml/main.py
+++ autogoal/experimental/sparseml/main.py
@@ -1,37 +1,45 @@
 from sparseml.keras.optim import ScheduledModifierManager
 from tensorflow.keras.utils import to_categorical
-from autogoal.contrib.keras  import KerasClassifier
+from autogoal.contrib.keras import KerasClassifier
 from autogoal.grammar import CategoricalValue
 
 
 def build_sparseml_keras_classifier(path_to_recipe: str):
     """Build custom KerasClassifier algorithm applaying sparcification techniques provided in path_to_recipe yaml file."""
-    class SparseMLKerasClassifier(KerasClassifier):
-        def __init__(self,optimizer: CategoricalValue("sgd", "adam", "rmsprop"), grammar=None, **kwargs) -> None:
-            self._path=path_to_recipe
-            self._manager=ScheduledModifierManager.from_yaml(self._path)
-            super().__init__(grammar=grammar or self._build_grammar(), optimizer=optimizer, **kwargs)
 
-        def fit(self,X,y):
+    class SparseMLKerasClassifier(KerasClassifier):
+        def __init__(
+            self,
+            optimizer: CategoricalValue("sgd", "adam", "rmsprop"),
+            grammar=None,
+            **kwargs
+        ) -> None:
+            self._path = path_to_recipe
+            self._manager = ScheduledModifierManager.from_yaml(self._path)
+            super().__init__(
+                grammar=grammar or self._build_grammar(), optimizer=optimizer, **kwargs
+            )
+
+        def fit(self, X, y):
             self._classes = {k: v for k, v in zip(set(y), range(len(y)))}
             self._inverse_classes = {v: k for k, v in self._classes.items()}
             y = [self._classes[yi] for yi in y]
             y = to_categorical(y)
 
-            #Create Model
+            # Create Model
             if self._graph is None:
-                raise TypeError("You must call `sample` to generate the internal model.")
+                raise TypeError(
+                    "You must call `sample` to generate the internal model."
+                )
 
             self._build_nn(self._graph, X, y)
 
-            #Sparcificate Model
+            # Sparcificate Model
             old_model = self._model
             self._model = self._manager.finalize(old_model)
             self._model.compile(**self._compile_kwargs)
 
-            #Fit Model
-            self._fit_model(X, y) 
-            
-    
+            # Fit Model
+            self._fit_model(X, y)
+
     return SparseMLKerasClassifier
-        
