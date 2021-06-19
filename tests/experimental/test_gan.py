5869056978ed382ae39e1192e8c5735b0710b9e5
Marcos Antonio Maceo Reyes <marcosmaceowork@gmail.com>
2021-06-19 01:00:27 -0400

    Add test for gan algorithm base types

---
+++ tests/experimental/test_gan.py
@@ -0,0 +1,26 @@
+from autogoal.kb import build_pipeline_graph, AlgorithmBase
+from autogoal.experimental.gan.gan import (
+    GAN,
+)
+from autogoal.contrib import find_classes
+from autogoal.experimental.gan.utils import ImageFile
+from autogoal.kb import Seq, Word, Supervised
+
+
+def test_algorithm_in_pipeline_graph():
+    pipelines = build_pipeline_graph(
+        input_types=([Seq[ImageFile], Seq[ImageFile]]),
+        output_type=ImageFile,
+        registry=find_classes() + [GAN],
+    )
+
+    assert GAN in pipelines.nodes()
+
+
+def test_algorithm_report_correct_types():
+    assert GAN.input_types() == (
+        Seq[ImageFile],
+        Seq[ImageFile],
+    )
+
+    assert GAN.output_type() == ImageFile
