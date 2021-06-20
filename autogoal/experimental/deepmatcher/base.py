b1107f1a256c32afda0e25dd0ff54ecd46665db2
dgc9715 <dgc9715@gmail.com>
2021-06-20 22:47:20 +0000

    add preprocessor, fix validation tests

--- autogoal/experimental/deepmatcher/base.py
+++ autogoal/experimental/deepmatcher/base.py
@@ -10,7 +10,6 @@ from autogoal.utils import nice_repr
 import deepmatcher
 from pathlib import Path
 import csv
-import pandas
 import random
 import string
 
@@ -29,28 +28,19 @@ def buildMatchingSet(file_name):
         embeddings_cache_path=(CACHE.parent / '.vector_cache')
     )
 
+def split_X(X):
+    p = int(80 / 100 * len(X))
+    return X[:p], X[p:]
+
 def random_string():
     return ''.join(random.choice(string.ascii_lowercase + string.digits) for _ in range(10))
 
-def split_X(X):
-    idx = 0
-    for row in X:
-        if row[0] == None:
-            break
-        idx += 1
-    
-    if idx+1 < len(X):
-        vX = X[idx+1:]
-        X = X[:idx]
-    else:
-        vX = None
-    return X, vX
-
 @nice_repr
-class SupervisedTextMatcher(AlgorithmBase): # add doc strings
+class SupervisedTextMatcher(AlgorithmBase):
     """
     
     Params:
+        -preprocessor: Add headers to datasets
         -attr_summarizer: The attribute summarizer. Takes in two word embedding sequences
             and summarizes the information in them to produce two summary vectors as output.
         -classifier: The neural network to perform match / mismatch classification based
@@ -60,11 +50,13 @@ class SupervisedTextMatcher(AlgorithmBase): # add doc strings
     """
     def __init__(
             self,
+            preprocessor: algorithm(Seq[Seq[Text]], Seq[Seq[Text]]),
             attr_summarizer: CategoricalValue('sif', 'rnn', 'attention', 'hybrid'),
             classifier: CategoricalValue('2-layer-highway', '2-layer-highway-tanh', '3-layer-residual', '3-layer-residual-relu'),
             epoch: DiscreteValue(min=5, max=100),
             label_smoothing: ContinuousValue(min=0.01, max=0.2)
         ):
+        self.preprocessor = preprocessor
         self.attr_summarizer = attr_summarizer
         self.classifier = classifier
         self.epoch = epoch
@@ -78,7 +70,7 @@ class SupervisedTextMatcher(AlgorithmBase): # add doc strings
     def eval(self):
         self._mode = "eval"
 
-    def run(self,  X:Seq[Seq[Text]], y:Supervised[VectorCategorical]) -> VectorCategorical: # fix annotations
+    def run(self,  X:Seq[Seq[Text]], y:Supervised[VectorCategorical]) -> VectorCategorical:
         if self._mode == "train":
             return self._train(X,y)
         elif self._mode == "eval":
@@ -90,6 +82,8 @@ class SupervisedTextMatcher(AlgorithmBase): # add doc strings
             X[i].insert(1, y[i])
 
         X, vX = split_X(X)
+        self.preprocessor.run(X)
+        self.preprocessor.run(vX)
         assert vX
 
         if not Path.exists(CACHE):
@@ -117,14 +111,7 @@ class SupervisedTextMatcher(AlgorithmBase): # add doc strings
 
     def _eval(self, X):
         X = [row.copy() for row in X]
-        # ans = []
-        try:
-            X, vX = split_X(X)
-            # X += vX[1:]
-        except:
-            pass
-
-        X[0].insert(1, 'label') # need to switch to process_unlabeled
+        self.preprocessor.run(X)
         for i in range(1, len(X)):
             X[i].insert(1, 0)
 
@@ -134,30 +121,27 @@ class SupervisedTextMatcher(AlgorithmBase): # add doc strings
             w.writerows(X)
 
         eval_dataset = buildMatchingSet(eval)
-        # eval_dataset = deepmatcher.data.process_unlabeled(path=CACHE / 'temp.val',
-        #     trained_model=self.model, ignore_columns=('left_id', 'right_id'))
-        
-        # with open(CACHE / 'temp.eval.txt', 'w') as f: # just to see logs
-        #     self.model.run_prediction(eval_dataset, True).to_csv(f)
-        
         remove(eval)
         return [1 if x >= 0.5 else 0 for x in self.model.run_prediction(eval_dataset).to_dict()['match_score'].values()]
-        # for x in X:
-        #     dataset = deepmatcher.data.MatchingDataset() # build from x
-        #     ans.append(self.model.run_eval(dataset=dataset)) # use self.model.run_prediction() instead ?
-        # return ans
 
 if __name__ == '__main__':
+    test_name = 'Fodors-Zagats'
+    from autogoal.experimental.deepmatcher.dataset import DeepMatcherDataset
+    from autogoal.experimental.deepmatcher import DATASETS
+    headers, X_train, y_train , X_test , y_test = DeepMatcherDataset(test_name, DATASETS[test_name]).load()
+    @nice_repr
+    class ProcessData(AlgorithmBase):
+        HEADERS=headers
+        def run(self, X: Seq[Seq[Text]]) -> Seq[Seq[Text]]:
+            X.insert(0, ProcessData.HEADERS)
+            return X
     s = SupervisedTextMatcher(
+        preprocessor=ProcessData(),
         attr_summarizer='attention',
         classifier='2-layer-highway',
         epoch=2,
         label_smoothing=0.02
     )
-    test_name = 'Fodors-Zagats'
-    from autogoal.experimental.deepmatcher.dataset import DeepMatcherDataset
-    from autogoal.experimental.deepmatcher import DATASETS
-    X_train, y_train , X_test , y_test = DeepMatcherDataset(test_name, DATASETS[test_name]).load()
     s.train()
     s.run(X_train, y_train)
     s.eval()
