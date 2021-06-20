d08c03970f4b2f9fb1cbcb6f8140d36f5f90ea17
dgc9715 <dgc9715@gmail.com>
2021-06-20 22:48:06 +0000

    fix test

--- tests/experimental/deepmatcher/test.py
+++ tests/experimental/deepmatcher/test.py
@@ -1,4 +1,4 @@
-from autogoal.kb import Sentence, Seq, Supervised ,VectorCategorical
+from autogoal.kb import Sentence, Seq, Supervised ,VectorCategorical, AlgorithmBase
 from autogoal.ml import AutoML
 from autogoal.contrib import find_classes
 from autogoal.utils import Min, Gb
@@ -6,18 +6,27 @@ from autogoal.kb import Seq, Word, Text, Sentence, MatrixContinuousDense, build_
 from autogoal.contrib import find_classes
 from autogoal.experimental.deepmatcher.base import SupervisedTextMatcher
 from autogoal.search import RichLogger
+from autogoal.utils import nice_repr
 
 from autogoal.experimental.deepmatcher.dataset import DeepMatcherDataset
 from autogoal.experimental.deepmatcher import DATASETS
 
+
 def test_supervised_text_matcher():
     test_name = 'Fodors-Zagats'
-    X_train, y_train , X_test , y_test = DeepMatcherDataset(test_name, DATASETS[test_name]).load()
+    headers, X_train, y_train , X_test , y_test = DeepMatcherDataset(test_name, DATASETS[test_name]).load()
+
+    @nice_repr
+    class ProcessData(AlgorithmBase):
+        HEADERS=headers
+        def run(self, X: Seq[Seq[Text]]) -> Seq[Seq[Text]]:
+            X.insert(0, ProcessData.HEADERS)
+            return X
 
     automl = AutoML(
-        input = (Seq[Seq[Text]], Supervised[VectorCategorical]), # fix annotations
-        output = VectorCategorical, # fix annotations
-        registry = [SupervisedTextMatcher] + find_classes(),
+        input = (Seq[Seq[Text]], Supervised[VectorCategorical]),
+        output = VectorCategorical,
+        registry = [SupervisedTextMatcher,ProcessData] + find_classes(),
         evaluation_timeout = 3 * Min,
         memory_limit = 3 * Gb,
         search_timeout = 10 * Min
@@ -29,12 +38,13 @@ def test_supervised_text_matcher():
 
 def test_supervised_text_matcher_pipeline():
     pipelines = build_pipeline_graph(
-        input_types = (Seq[Seq[Text]], Supervised[VectorCategorical]),  # fix annotations
-        output_type = VectorCategorical,  # fix annotations
+        input_types = (Seq[Seq[Text]], Supervised[VectorCategorical]),
+        output_type = VectorCategorical,
         registry = find_classes() + [SupervisedTextMatcher],
     )
     nodes = pipelines.nodes()
     assert SupervisedTextMatcher in nodes
 
 if __name__ == '__main__':
+    # test_supervised_text_matcher_pipeline() # add ProcessData in registry
     test_supervised_text_matcher()
