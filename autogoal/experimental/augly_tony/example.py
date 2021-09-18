4aab4a8b9e4c234453fa1123b2dd8f78c35fb01e
Tony Raul Blanco Fernandez <70nybl4nc0@gmail.com>
2021-09-18 01:51:45 -0400

    Add example using string transformers

---
+++ autogoal/experimental/augly_tony/example.py
@@ -0,0 +1,33 @@
+from autogoal.kb import AlgorithmBase
+
+from autogoal.grammar import BooleanValue, DiscreteValue
+from autogoal.kb import *
+
+from autogoal.ml import AutoML
+from autogoal.contrib import find_classes
+
+from autogoal.datasets import haha
+
+from transformers._text import (
+    ReplaceSimilarCharsTransformer,
+    InsertPunctuationCharsTransformer,
+    SplitWordsTransformer,
+)
+
+X_train, y_train, X_test, y_test = haha.load()
+
+automl = AutoML(
+    input=(Seq[Sentence], Supervised[VectorCategorical]),  # **tipos de entrada**
+    output=VectorCategorical,
+    registry=[
+        ReplaceSimilarCharsTransformer,
+        InsertPunctuationCharsTransformer,
+        SplitWordsTransformer,
+    ]
+    + find_classes(),
+)
+
+automl.fit(X_train, y_train)
+
+score = automl.score(X_test, y_test)
+print(score)
