8663f4bd93fea5899859e8b5cec8b73ce53ecf87
Carlos Bermudez <cbermudez.dev@gmail.com>
2021-10-11 17:25:54 -0400

    Add test for keras layers from config class method

--- tests/contrib/test_keras.py
+++ tests/contrib/test_keras.py
@@ -17,6 +17,26 @@ from autogoal.contrib.keras import (
     KerasSequenceTagger,
 )
 
+from autogoal.contrib.keras._generated import (
+    Activation,
+    Conv1D,
+    Conv2D,
+    Dense,
+    BatchNormalization,
+    Dropout,
+    Embedding,
+    Flatten,
+    MaxPooling2D,
+    Reshape2D,
+    Seq2SeqLSTM,
+    Seq2VecBiLSTM,
+    Seq2VecLSTM,
+    Seq2SeqBiLSTM,
+    TimeDistributed,
+)
+
+from tensorflow.keras.activations import softmax
+
 
 def test_matrix_classification_pipeline_uses_keras_classifier():
     pipelines = build_pipeline_graph(
@@ -50,3 +70,42 @@ def test_algorithms_report_correct_types():
         Seq[MatrixContinuousDense],
         Supervised[Seq[Seq[Label]]],
     )
+
+
+def test_keras_layers_from_config():
+    activation = Activation(softmax)
+    conv1D = Conv1D(32, 3)
+    conv2D = Conv2D(32, 3, 0.0005, 0.0005)
+    dense = Dense(32)
+    batchNormalization = BatchNormalization()
+    dropout = Dropout(0.3)
+    embedding = Embedding(32)
+    flatten = Flatten()
+    maxPooling2D = MaxPooling2D()
+    rashape = Reshape2D()
+    seq2SeqLSTM = Seq2SeqLSTM(32, "linear", "linear", 0.25, 0.25)
+    seq2VecBiLSTM = Seq2VecBiLSTM("sum", 32, "linear", "linear", 0.25, 0.25)
+    seq2VecLSTM = Seq2VecLSTM(32, "linear", "linear", 0.25, 0.25)
+    seq2SeqBiLSTM = Seq2SeqBiLSTM("sum", 32, "linear", "linear", 0.25, 0.25)
+    timeDistributed = TimeDistributed(dense)
+
+    layers = [
+        activation,
+        conv1D,
+        conv2D,
+        dense,
+        batchNormalization,
+        dropout,
+        embedding,
+        flatten,
+        maxPooling2D,
+        rashape,
+        seq2SeqLSTM,
+        seq2VecBiLSTM,
+        seq2VecLSTM,
+        seq2SeqBiLSTM,
+        timeDistributed,
+    ]
+
+    for layer in layers:
+        layer.__class__.from_config(layer.get_config())
