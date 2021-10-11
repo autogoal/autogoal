f16fe6d4e0ccf76f7dc7e37a6d043db426a868d5
Carlos Bermudez <cbermudez.dev@gmail.com>
2021-10-11 18:54:47 -0400

    Change keras layers wrappers from config test

--- tests/contrib/test_keras.py
+++ tests/contrib/test_keras.py
@@ -35,8 +35,6 @@ from autogoal.contrib.keras._generated import (
     TimeDistributed,
 )
 
-from tensorflow.keras.activations import softmax
-
 
 def test_matrix_classification_pipeline_uses_keras_classifier():
     pipelines = build_pipeline_graph(
@@ -73,39 +71,38 @@ def test_algorithms_report_correct_types():
 
 
 def test_keras_layers_from_config():
-    activation = Activation(softmax)
-    conv1D = Conv1D(32, 3)
-    conv2D = Conv2D(32, 3, 0.0005, 0.0005)
-    dense = Dense(32)
-    batchNormalization = BatchNormalization()
-    dropout = Dropout(0.3)
-    embedding = Embedding(32)
-    flatten = Flatten()
-    maxPooling2D = MaxPooling2D()
-    rashape = Reshape2D()
-    seq2SeqLSTM = Seq2SeqLSTM(32, "linear", "linear", 0.25, 0.25)
-    seq2VecBiLSTM = Seq2VecBiLSTM("sum", 32, "linear", "linear", 0.25, 0.25)
-    seq2VecLSTM = Seq2VecLSTM(32, "linear", "linear", 0.25, 0.25)
-    seq2SeqBiLSTM = Seq2SeqBiLSTM("sum", 32, "linear", "linear", 0.25, 0.25)
-    timeDistributed = TimeDistributed(dense)
-
-    layers = [
-        activation,
-        conv1D,
-        conv2D,
-        dense,
-        batchNormalization,
-        dropout,
-        embedding,
-        flatten,
-        maxPooling2D,
-        rashape,
-        seq2SeqLSTM,
-        seq2VecBiLSTM,
-        seq2VecLSTM,
-        seq2SeqBiLSTM,
-        timeDistributed,
-    ]
-
-    for layer in layers:
+    def test_from_config(layer):
         layer.__class__.from_config(layer.get_config())
+
+    activation = Activation("softmax")
+    test_from_config(activation)
+    conv1D = Conv1D(32, 3)
+    test_from_config(conv1D)
+    conv2D = Conv2D(32, 3, 0.0005, 0.0005)
+    test_from_config(conv2D)
+    dense = Dense(32)
+    test_from_config(dense)
+    batchNormalization = BatchNormalization()
+    test_from_config(batchNormalization)
+    dropout = Dropout(0.3)
+    test_from_config(dropout)
+    embedding = Embedding(32)
+    test_from_config(embedding)
+    flatten = Flatten()
+    test_from_config(flatten)
+    maxPooling2D = MaxPooling2D()
+    test_from_config(maxPooling2D)
+    rashape = Reshape2D()
+    test_from_config(rashape)
+    seq2SeqLSTM = Seq2SeqLSTM(32, "linear", "linear", 0.25, 0.25)
+    test_from_config(seq2SeqLSTM)
+    seq2VecBiLSTM = Seq2VecBiLSTM("sum", 32, "linear", "linear", 0.25, 0.25)
+    test_from_config(seq2VecBiLSTM)
+    seq2VecLSTM = Seq2VecLSTM(32, "linear", "linear", 0.25, 0.25)
+    test_from_config(seq2VecLSTM)
+    seq2SeqBiLSTM = Seq2SeqBiLSTM("sum", 32, "linear", "linear", 0.25, 0.25)
+    test_from_config(seq2SeqBiLSTM)
+    timeDistributed = TimeDistributed(dense)
+    test_from_config(timeDistributed)
+
+
