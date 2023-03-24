5825355a7ff34d80a617dbc7c472ab8b0c8e8c0d
Ernesto Estevanell <ernestoestevanellv@gmail.com>
2023-03-24 17:33:11 -0400

    fixed dockerfiles invoking install-package.sh without sudo error

--- dockerfiles/install-package.sh
+++ dockerfiles/install-package.sh
@@ -3,7 +3,7 @@ set -e
 
 arg=$1 # argument must be either core, remote, common, or a contrib identifier
 
-echo "Trying to install autogoal-$arg"
+echo "Trying to install autogoal_$arg"
 poetry config virtualenvs.create false
 case $arg in
     core)
@@ -15,14 +15,14 @@ case $arg in
         cd /home/coder/autogoal && pip install -e autogoal-remote
     ;;
     common)
-        cd /home/coder/autogoal/autogoal-contrib/common && poetry install
-        cd /home/coder/autogoal/autogoal-contrib && pip install -e autogoal-contrib-common
+        cd /home/coder/autogoal/autogoal-contrib/autogoal_contrib && poetry install
+        cd /home/coder/autogoal/autogoal-contrib && pip install -e autogoal_contrib
     ;;
     sklearn | nltk)
-        cd "/home/coder/autogoal/autogoal-contrib/autogoal-$arg" && poetry install
-        cd "/home/coder/autogoal/autogoal-contrib" && pip install -e "autogoal-$arg"
+        cd "/home/coder/autogoal/autogoal-contrib/autogoal_$arg" && poetry install
+        cd "/home/coder/autogoal/autogoal-contrib" && pip install -e "autogoal_$arg"
     ;;
     *)
-        echo "No supported AutoGOAL package with name 'autogoal-$arg'"
+        echo "No supported AutoGOAL package with name 'autogoal_$arg'"
     ;;
 esac
