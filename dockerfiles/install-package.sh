0ae430f0f3e77487769416fb4a96de4b33bfd5ec
Ernesto Estevanell <ernestoestevanellv@gmail.com>
2023-10-13 12:45:47 +0200

    Updated dockerfile core

--- dockerfiles/install-package.sh
+++ dockerfiles/install-package.sh
@@ -9,20 +9,20 @@ do
     echo "Trying to install autogoal_$arg"
     case $arg in
         core)
-            cd /home/coder/autogoal/autogoal && poetry install
+            cd /home/coder/autogoal/autogoal && sudo poetry install
             cd /home/coder/autogoal && pip install -e autogoal
         ;;
         remote)
-            cd /home/coder/autogoal/autogoal-remote && poetry install
-            cd /home/coder/autogoal && pip install -e autogoal-remote
+            cd /home/coder/autogoal/autogoal-remote && sudo poetry install
+            cd /home/coder/autogoal && sudo pip install -e autogoal-remote
         ;;
         common)
-            cd /home/coder/autogoal/autogoal-contrib/autogoal_contrib && poetry install
-            cd /home/coder/autogoal/autogoal-contrib && pip install -e autogoal_contrib
+            cd /home/coder/autogoal/autogoal-contrib/autogoal_contrib && sudo poetry install
+            cd /home/coder/autogoal/autogoal-contrib && sudo pip install -e autogoal_contrib
         ;;
         *)
-            cd "/home/coder/autogoal/autogoal-contrib/autogoal_$arg" && poetry install
-            cd "/home/coder/autogoal/autogoal-contrib" && pip install -e "autogoal_$arg"
+            cd "/home/coder/autogoal/autogoal-contrib/autogoal_$arg" && sudo poetry install
+            cd "/home/coder/autogoal/autogoal-contrib" && sudo pip install -e "autogoal_$arg"
         ;;
     esac
 done
