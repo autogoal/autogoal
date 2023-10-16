ca09eb1b5e85e0afa257ee0c4e4788a335730360
Ernesto Estevanell <ernestoestevanellv@gmail.com>
2023-10-16 10:32:26 +0200

    added submodules checkout. fixed generate_full_image script

--- generate_full_image.ps1
+++ generate_full_image.ps1
@@ -1,2 +1,11 @@
-$contribs = @("sklearn", "nltk", "gensim", "regex", "keras", "spacy", "streamlit", "telegram", "transformers", "wikipedia")
-docker build . -t autogoal/autogoal:all-contribs -f dockerfiles/development/dockerfile --build-arg extras="common $contribs remote" --no-cache
+# Change directory to autogoal-contrib/
+Set-Location -Path .\autogoal-contrib\
+
+# Get the list of directories starting with 'autogoal_' and exclude 'autogoal_contrib'
+$contribs = Get-ChildItem -Directory | Where-Object { $_.Name -like "autogoal_*" -and $_.Name -ne "autogoal_contrib" } | ForEach-Object { $_.Name.Replace("autogoal_", "") }
+
+# Change back to the original directory
+Set-Location -Path ..
+
+# Build the Docker image
+docker build . -t autogoal/autogoal:all-contribs -f dockerfiles/development/dockerfile --build-arg extras="common $($contribs -join ' ') remote" --no-cache
--- generate_full_image.sh
+++ generate_full_image.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-contribs="common remote $(cd autogoal-contrib/ && ls -d autogoal_* | grep -v 'autogoal_contrib' | sed 's/autogoal_//')"
+contribs="$(cd autogoal-contrib/ && ls -d autogoal_* | grep -v 'autogoal_contrib' | sed 's/autogoal_//')"
 docker build . -t autogoal/autogoal:all-contribs -f dockerfiles/development/dockerfile --build-arg extras="common $contribs remote" --no-cache
