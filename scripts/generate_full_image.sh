29155e0dae113c45a449f9c39ee25fa8f507b738
Ernesto Estevanell <ernestoestevanellv@gmail.com>
2023-10-18 11:49:00 +0200

    changed workflow

--- scripts/generate_full_image.sh
+++ scripts/generate_full_image.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 contribs="$(cd autogoal-contrib/ && ls -d autogoal_* | grep -v 'autogoal_contrib' | sed 's/autogoal_//')"
-docker build . -t autogoal/autogoal:all-contribs -f dockerfiles/development/dockerfile --build-arg extras="common $contribs remote" --no-cache
+docker build . -t autogoal/autogoal:all-contribs-latest -f dockerfiles/development/dockerfile --build-arg extras="common $contribs remote" --no-cache2
