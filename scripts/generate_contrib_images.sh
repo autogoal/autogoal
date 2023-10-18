d1322bd0767862235d16a92dfc6c8768a755fd5c
Ernesto Estevanell <ernestoestevanellv@gmail.com>
2023-10-18 12:32:57 +0200

    updated generate_contrib_images script

--- scripts/generate_contrib_images.sh
+++ scripts/generate_contrib_images.sh
@@ -19,7 +19,7 @@ contribs="$(cd autogoal-contrib/ && ls -d autogoal_* | grep -v 'autogoal_contrib
 
 for contrib in "${contribs[@]}"
 do
-  make docker-contrib CONTRIB="$contrib"
+  docker build . -t autogoal/autogoal:$contrib-latest -f dockerfiles/development/dockerfile --build-arg extras="common $contrib remote" --no-cache
   if [ "$push_images" -eq 1 ]; then
     docker push autogoal/autogoal:$contrib
   fi
