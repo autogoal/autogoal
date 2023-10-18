1c358e5467e05b06dbf7dd5c28a3edd77227d0ca
Ernesto Estevanell <ernestoestevanellv@gmail.com>
2023-10-18 10:42:35 +0200

    fixed docker generation scripts

--- scripts/generate_contrib_images.ps1
+++ scripts/generate_contrib_images.ps1
@@ -1,5 +1,14 @@
-$contribs = @("sklearn", "nltk", "gensim", "regex", "keras", "spacy", "streamlit", "telegram", "transformers", "wikipedia")
+param (
+    [switch]$p = $false
+)
 
-foreach ($contrib in $contribs) {
-  make docker-contrib CONTRIB="$contrib"
+$contribs = Get-ChildItem -Path autogoal-contrib/ -Filter autogoal_* | Where-Object { $_.Name -ne 'autogoal_contrib' } | ForEach-Object { $_.Name.Replace('autogoal_', '') }
+
+foreach ($contrib in $contribs)
+{
+    make docker-contrib CONTRIB=$contrib
+    if ($p)
+    {
+        docker push autogoal/autogoal:$contrib
+    }
 }
--- scripts/generate_contrib_images.sh
+++ scripts/generate_contrib_images.sh
@@ -1,6 +1,26 @@
 #!/bin/bash
+
+# Initialize our own variables
+push_images=0
+
+# Parse command-line options
+while getopts ":p" opt; do
+  case ${opt} in
+    p)
+      push_images=1
+      ;;
+    \?)
+      echo "Invalid option: -$OPTARG" >&2
+      ;;
+  esac
+done
+
 contribs="$(cd autogoal-contrib/ && ls -d autogoal_* | grep -v 'autogoal_contrib' | sed 's/autogoal_//')"
+
 for contrib in "${contribs[@]}"
 do
   make docker-contrib CONTRIB="$contrib"
+  if [ "$push_images" -eq 1 ]; then
+    docker push autogoal/autogoal:$contrib
+  fi
 done
