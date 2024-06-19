23b2029f659d97d5aaa18cfa34b1286923bbc7f4
Ernesto Estevanell <ernestoestevanellv@gmail.com>
2024-06-19 18:55:06 +0000

    fixed yelp reviews dataset loader

--- autogoal/autogoal/datasets/yelp_reviews.py
+++ autogoal/autogoal/datasets/yelp_reviews.py
@@ -20,20 +20,29 @@ def load():
     y_test = []
     with open(path / "train.csv", "r") as fd:
         reader = csv.reader(fd)
+        title_line = True
         for row in reader:
+            if title_line:
+                title_line = False
+                continue
             X_train.append(row[1])
-            y_train.append(row[0])
+            y_train.append(int(row[0]))
             
     with open(path / "test.csv", "r") as fd:
         reader = csv.reader(fd)
+        title_line = True
         for row in reader:
+            if title_line:
+                title_line = False
+                continue
             X_test.append(row[1])
-            y_test.append(row[0])
+            y_test.append(int(row[0]))
     
     assert len(X_train) == len(y_train)
     assert len(X_test) == len(y_test)
-
+    
     return X_train, y_train, X_test, y_test
+
 
 if __name__ == "__main__":
     load()
